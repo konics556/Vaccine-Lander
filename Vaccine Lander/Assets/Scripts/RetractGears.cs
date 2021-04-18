c4681bc6dac47d6c709104c3c60456884e475038
particlesmoke <f20180468@pilani.bits-pilani.ac.in>
2021-04-18 14:29:37 +0530

    gear retraction working

--- Vaccine Lander/Assets/Scripts/RetractGears.cs
+++ Vaccine Lander/Assets/Scripts/RetractGears.cs
@@ -6,6 +6,8 @@ public class RetractGears : MonoBehaviour
 {
     // Start is called before the first frame update
     public PolygonCollider2D bb;
+    public SliderJoint2D LeftGear;
+    public SliderJoint2D RightGear;
     void Start()
     {
         
@@ -15,11 +17,21 @@ public class RetractGears : MonoBehaviour
     void FixedUpdate()
     {
        // Debug.Log(Physics2D.Raycast(transform.position, -Vector2.up));
-        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y - bb.bounds.extents.y), -Vector2.up, 10);
-        Debug.Log(hit.collider.name);
-        //if (hit.collider.name == "landingPad")
-        //{
-        //    Debug.Log("we're there");
-        //}
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y - bb.bounds.extents.y), -Vector2.up, 4);
+        //Debug.Log(hit.collider.name);
+        try
+        {
+            if (hit.collider.tag == "Surface")
+            {
+                Debug.Log("we're there");
+                LeftGear.useMotor = true;
+                RightGear.useMotor = true;
+            }
+        }
+        catch
+        {
+            LeftGear.useMotor = false;
+            RightGear.useMotor = false;
+        }
     }
 }
